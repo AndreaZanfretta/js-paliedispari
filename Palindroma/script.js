02701c60bbdd6546491c952a9812dca8e2054416
AndreaZanfretta <andrea.zanfretta.97@gmail.com>
2022-04-01 15:07:37 +0200

    palindroma finito

--- Palindroma/script.js
+++ Palindroma/script.js
@@ -3,3 +3,17 @@ Creare una funzione per capire se la parola inserita è palindroma */
 
 let parola = prompt("Inserisci una parola e scopri se è palindroma!");
 
+function palindromo(parola) {
+    let lettere = parola.split("");
+    let reverse = "";
+    for(let i = lettere.length -1; i>= 0; i--){
+        reverse += lettere[i];
+    }
+    if(parola === reverse){
+        return alert("La parola è palindroma")
+    } else {
+        return alert("La parola non è palindroma")
+    }
+}
+let palindroma = palindromo(parola);
+console.log(palindroma)
