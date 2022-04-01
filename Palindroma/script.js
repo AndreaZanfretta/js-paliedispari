7030356d8d3d6e8f42761b4960ee69b78c273fae
AndreaZanfretta <andrea.zanfretta.97@gmail.com>
2022-04-01 14:56:24 +0200

    struttura creata

---
+++ Palindroma/script.js
@@ -0,0 +1,5 @@
+/* Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma */
+
+let parola = prompt("Inserisci una parola e scopri se è palindroma!");
+
